5405452fd301e9a0a2119f80c3ca78d0333be82b
Prajwal V <vprajwal2204@gmail.com>
2026-08-25 21:20:39 +0530

    Test pure python http server to diagnose FastAPI crash

--- api/ping.py
+++ api/ping.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/api/ping")
-def ping():
-    return {"message": "pong"}
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello from pure python'.encode('utf-8'))
